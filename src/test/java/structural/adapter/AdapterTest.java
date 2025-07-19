be477620747ec595af66d1b20aca01da28bec0c8
Julio Cesar Roque <jroque@cobuys.com>
2025-07-19 19:48:53 -0400

    Add a Bridge design pattern

--- src/test/java/structural/adapter/AdapterTest.java
+++ src/test/java/structural/adapter/AdapterTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("Adapter Pattern")
+@DisplayName("Adapter Design Pattern")
 class AdapterTest {
     RoundHole hole;
     RoundPeg roundPeg;
